a71cbafcc5dbc24cd49a8f0a377f2affe614b7c0
pd222dj <pd222dj@student.lnu.se>
2018-01-28 23:24:30 +0100

    Added source-code for power library

---
+++ libs/power/power.c
@@ -0,0 +1,10 @@
+// Author: Peter Danielsson
+#include "power.h"
+
+float calc_power_r(float volt, float resistance) {
+    return volt * volt / resistance;
+}
+
+float calc_power_i(float volt, float current) {
+    return volt * current;
+}
---
+++ libs/power/power.h
@@ -0,0 +1,8 @@
+// Author: Peter Danielsson
+#ifndef _POWER_H_
+#define _POWER_H_
+
+float calc_power_r(float volt, float resistance);
+float calc_power_i(float volt, float current);
+
+#endif
